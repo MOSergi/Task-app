5db5e8c04ec4b1a579380dbc63cd31197824e4cf
MOSergi <sergioorozmendez@gmail.com>
2024-05-26 08:50:22 +0200

    corregidos errores configuracion docker

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 export function dbConnectionConfig(){
     const sequelize = new Sequelize({
-        host: process.env.ENVIROMENT_MODE === 'Dev' ? 'localhost': 'task-app-container',
+        host: process.env.ENVIROMENT_MODE === 'Dev' ? 'localhost': 'mysql-task-container',
         database : process.env.MYSQL_DATABASE,
         port: 3306,
         username : process.env.MYSQL_USER,
