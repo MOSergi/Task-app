7dcb3e53d6c6c63d1ebb2e06c172f718fb53ab8d
MOSergi <sergioorozmendez@gmail.com>
2024-06-16 13:38:22 +0200

    optimizado contenedor dependencias para usar la misma instancia

--- server/src/common/dependency_container/CustomeDependencyContainer.ts
+++ server/src/common/dependency_container/CustomeDependencyContainer.ts
@@ -2,11 +2,10 @@ import { UsersRepository } from "../../modules/Users/repository/users.repository
 import { UsersActionService } from "../../modules/Users/services/users.action.service";
 import { UsersReadService } from "../../modules/Users/services/users.read.service";
 
+const usersRepository = new UsersRepository();
+const usersReadService = new UsersReadService(usersRepository);
+const usersActionService = new UsersActionService(usersRepository, usersReadService);
+
 export const CustomDependencyContainer = {
-    usersActionService : new UsersActionService(
-        new UsersRepository(),
-        new UsersReadService(
-            new UsersRepository()
-        )
-    ),
+    usersActionService : usersActionService,
 }
