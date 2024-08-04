c750cc0343ea0f03ca2d0ba06e904b6578f50cde
MOSergi <sergioorozmendez@gmail.com>
2024-08-04 17:36:31 +0200

    arreglado problema de tipado

--- server/src/modules/Users/services/interfaces/UsersActionService.ts
+++ server/src/modules/Users/services/interfaces/UsersActionService.ts
@@ -2,4 +2,4 @@ import { ActionService } from "../../../../common/interfaces/ActionService";
 import { User } from "../../model/User.interface";
 import { CreateUserParams } from "./CreateUserParams";
 
-export interface UsersActionService extends ActionService<User, CreateUserParams> {}
+export interface UsersActionService extends ActionService<User, CreateUserParams, any> {}
